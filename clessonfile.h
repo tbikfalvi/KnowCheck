b9a9c6905e2c972b17deb21b1bbcc6dd4b0649e2
Tamas Bikfalvi <tbikfalvi@gmail.com>
2012-01-19 23:37:32 +0100

    ...

--- clessonfile.h
+++ clessonfile.h
@@ -2,6 +2,7 @@
 #define CLESSONFILE_H
 
 #include <QString>
+#include <vector>
 
 class cLessonRecord
 {
@@ -13,22 +14,22 @@ public:
                    const QString p_qsSelection="" );
     ~cLessonRecord();
 
-    QString     question() const;
-    void        setQuestion( const QString p_qsQuestion );
-    QString     answer() const;
-    void        setAnswer( const QString p_qsAnswer );
-    int         answerType() const;
-    void        setAnswerType( const int p_nAnswerType=1 );
-    QString     selection() const;
-    void        setSelection( const QString p_qsSelection="" );
+    QString                 question() const;
+    void                    setQuestion( const QString p_qsQuestion );
+    QString                 answer() const;
+    void                    setAnswer( const QString p_qsAnswer );
+    int                     answerType() const;
+    void                    setAnswerType( const int p_nAnswerType=1 );
+    QString                 selection() const;
+    void                    setSelection( const QString p_qsSelection="" );
 
-    QString     toString() const;
+    QString                 toString() const;
 
 private:
-    QString     m_qsQuestion;
-    QString     m_qsAnswer;
-    int         m_nAnswerType;
-    QString     m_qsSelection;
+    QString                 m_qsQuestion;
+    QString                 m_qsAnswer;
+    int                     m_nAnswerType;
+    QString                 m_qsSelection;
 };
 
 class cLessonFile
@@ -37,13 +38,14 @@ public:
     cLessonFile();
     ~cLessonFile();
 
-    void        setLessonFileName( const QString p_qsFileName );
+    void                    setLessonFileName( const QString p_qsFileName );
 
 private:
 
-    void        _createLessonFile();
+    void                    _createLessonFile();
 
-    QString     m_qsFileName;
+    vector<cLessonRecord*>  m_obRecords;
+    QString                 m_qsFileName;
 };
 
 #endif // CLESSONFILE_H
